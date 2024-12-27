0ce9fa288632e578e6473316847b44801d72963d
Will Snyder <willsnyder99@gmail.com>
2024-12-27 12:52:39 -0800

    Update TartarusJoystick.cpp
    Improved key press handling. Now keys have a flag that is tracked to detect when the keys change state. Now only the keys are pressed once

--- src/TartarusJoystick.cpp
+++ src/TartarusJoystick.cpp
@@ -7,13 +7,21 @@
 TartarusJoystick::TartarusJoystick() {
     // Placeholder calibration settings load, will be read from SAM D21 EEPROM.
     // Consider adding EEPROM to future revision.
-    this->_settings = JoystickSettings{{41, 12},
-                                       {1023, 1021},
-                                       {512, 512},
-                                       51, 51, 1.0};
+    this->m_settings = JoystickSettings{{56, 19},
+                                        {1023, 1010},
+                                        {571, 526},
+                                        51, 51, 1.0};
 
     // Initialize data variable
-    this->_data = {{0, 0}, false};
+    this->m_data = {{0, 0}, false};
+
+    // Initialize keys
+    this->m_keys[0] = {NAN, NAN, KEY_BUTTON, false}; // Button is special case
+    this->m_keys[1] = {1.17, 5.08, KEY_LEFT, false}; // Left is special case due to singularity
+    this->m_keys[2] = {0.39, 2.74, KEY_UP, false};
+    this->m_keys[3] = {1.96, 4.30, KEY_RIGHT, false};
+    this->m_keys[4] = {3.52, 5.89, KEY_DOWN, false};
+
     Keyboard.begin(); // Start keyboard for keystroke capability
 }
 
@@ -27,33 +35,33 @@ void TartarusJoystick::readJoystickData() {
     yVal = (int16_t) analogRead(A3);
 
     // Apply calibration to X and Y values
-    xVal = (int16_t) map(xVal, this->_settings.minPos.x, this->_settings.maxPos.x,
+    xVal = (int16_t) map(xVal, this->m_settings.minPos.x, this->m_settings.maxPos.x,
                          0, 1023);
 
-    yVal = (int16_t) map(yVal, this->_settings.minPos.y, this->_settings.maxPos.y,
+    yVal = (int16_t) map(yVal, this->m_settings.minPos.y, this->m_settings.maxPos.y,
                          0, 1023);
 
-    this->_applySmoothing(xVal, yVal);
-    this->_applyDeadzone(xVal, yVal);
+    this->m_applySmoothing(xVal, yVal);
+    this->m_applyDeadzone(xVal, yVal);
 
     // Map X and Y values to [-100, 100] with deadzones
     xVal = (int16_t) map(xVal, 0, 1023, JOYSTICK_MINIMUM, JOYSTICK_MAXIMUM);
     yVal = (int16_t) map(yVal, 0, 1023, JOYSTICK_MINIMUM, JOYSTICK_MAXIMUM);
 
-    this->_data.position.x = constrain(xVal, JOYSTICK_MINIMUM, JOYSTICK_MAXIMUM);
-    this->_data.position.y = constrain(yVal, JOYSTICK_MINIMUM, JOYSTICK_MAXIMUM);
+    this->m_data.position.x = constrain(xVal, JOYSTICK_MINIMUM, JOYSTICK_MAXIMUM);
+    this->m_data.position.y = constrain(yVal, JOYSTICK_MINIMUM, JOYSTICK_MAXIMUM);
 
-    this->_data.button = digitalRead(JOYSTICK_BUTTON);
+    this->m_data.button = digitalRead(JOYSTICK_BUTTON);
 }
 
 void TartarusJoystick::printToSerial() const {
     Serial.print("Position (x, y): ");
-    this->_data.position.printToSerial();
+    this->m_data.position.printToSerial();
     Serial.print(" Button: ");
-    Serial.println(this->_data.button);
+    Serial.println(this->m_data.button);
 
     Serial.print("Angle (rad): ");
-    Serial.println(atan2(this->_data.position.y, this->_data.position.x) + M_PI);
+    Serial.println(atan2(this->m_data.position.y, this->m_data.position.x) + M_PI);
 }
 
 // Point printToSerial() implementation
@@ -65,24 +73,24 @@ void Point::printToSerial() const {
     Serial.print(")");
 }
 
-void TartarusJoystick::_applyDeadzone(int16_t & xVal, int16_t & yVal) const {
-    if(abs(xVal - this->_settings.origin.x) <= this->_settings.xDeadzone) {
+void TartarusJoystick::m_applyDeadzone(int16_t & xVal, int16_t & yVal) const {
+    if(abs(xVal - this->m_settings.origin.x) <= this->m_settings.xDeadzone) {
         xVal = 512;
     }
 
-    if(abs(yVal - this->_settings.origin.y) <= this->_settings.yDeadzone) {
+    if(abs(yVal - this->m_settings.origin.y) <= this->m_settings.yDeadzone) {
         yVal = 512;
     }
 }
 
 // Scale sensitivity dependent on the distance from the origin
 // Range between 0.1 - 2.0.
-void TartarusJoystick::_applySmoothing(int16_t & xVal, int16_t & yVal) const {
-    xVal = (int16_t)(this->_settings.origin.x +
-            (xVal - this->_settings.origin.x) *
-            this->_settings.sensitivityScaling);
+void TartarusJoystick::m_applySmoothing(int16_t & xVal, int16_t & yVal) const {
+    xVal = (int16_t)(this->m_settings.origin.x +
+                     (xVal - this->m_settings.origin.x) *
+                     this->m_settings.sensitivityScaling);
 
-    yVal = (int16_t)(this->_settings.origin.y +
-            (yVal - this->_settings.origin.y) *
-            this->_settings.sensitivityScaling);
+    yVal = (int16_t)(this->m_settings.origin.y +
+                     (yVal - this->m_settings.origin.y) *
+                     this->m_settings.sensitivityScaling);
 }
